39ac0bdc39c32d445b122b041774a21167157358
tamanh0 <huynhtrantamanh5@gmail.com>
2025-06-09 23:24:49 +0700

    Update elements.js

--- src/data/elements.js
+++ src/data/elements.js
@@ -3,6 +3,10 @@ const elements = [
   { symbol: "H", name: "Hydrogen", number: 1 },
   { symbol: "He", name: "Helium", number: 2 },
   { symbol: "Li", name: "Lithium", number: 3 },
+  { symbol: “Be”, name: “Beryllium”, number: 4 }, 
+  { symbol: “B”, name: “Boron”, number: 5 }, 
+  { symbol: “C”, name: “Carbon”, number: 6 }, 
+  { symbol: “N”, name: “Nitrogen”, number: 7}, 
 ];
 
 export default elements;
